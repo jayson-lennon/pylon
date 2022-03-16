622f297ebd2295fb0fd70d4b9a4ad2cb9b0ecc16
jayson <>
2022-03-16 05:28:24 +0000

    Add function to traverse tera templates
    FossilOrigin-Name: 95298c5ef46a799bbfce10e58c4fc0aa9359481cca4f7364e5322ff2e368550d

--- pipeworks/src/lib/discover.rs
+++ pipeworks/src/lib/discover.rs
@@ -50,3 +50,62 @@ pub fn get_all_paths<P: AsRef<Path>>(
     }
     Ok(paths)
 }
+
+pub fn get_template_paths_for_content_path<P: AsRef<Path>, S: AsRef<str>>(
+    content_path: P,
+    template_root: P,
+    template_name: S,
+) -> Vec<PathBuf> {
+    let content_path = content_path.as_ref();
+    let template_root = template_root.as_ref();
+    let template_name = template_name.as_ref();
+
+    let mut template_path = {
+        let mut template_path = PathBuf::from(template_root);
+        // build a path with the template root as the first directory
+        for p in content_path.iter().skip(1) {
+            template_path.push(p);
+        }
+        // add the target template name
+        template_path.push(template_name);
+        template_path
+    };
+    let mut paths = vec![];
+    loop {
+        paths.push(template_path.clone());
+
+        template_path.pop();
+        if !template_path.pop() {
+            break;
+        }
+
+        if template_path.as_path() == template_root {
+            break;
+        }
+        template_path.push(template_name);
+    }
+    paths
+}
+
+#[cfg(test)]
+mod test {
+    use super::get_template_paths_for_content_path;
+    use std::path::PathBuf;
+
+    #[test]
+    fn gets_list_of_template_paths_for_given_content_path() {
+        let content_path = PathBuf::from("src/blog/post1");
+        let template_root = PathBuf::from("templates");
+        let template_name = "single.tera";
+        let template_paths =
+            get_template_paths_for_content_path(content_path, template_root, template_name);
+        assert_eq!(
+            template_paths[0],
+            PathBuf::from("templates/blog/post1/single.tera")
+        );
+        assert_eq!(
+            template_paths[1],
+            PathBuf::from("templates/blog/single.tera")
+        );
+    }
+}
