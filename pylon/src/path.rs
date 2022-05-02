3ee9fb28e9eb645e7d00e0615e4d396fc59923d5
Jayson Lennon <jayson@jaysonlennon.dev>
2022-05-02 16:57:12 -0700

    Add path types

--- pylon/src/path.rs
+++ pylon/src/path.rs
@@ -1,3 +1,4 @@
+use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
@@ -86,14 +87,24 @@ impl SysPath {
         }
     }
 
+    pub fn file_name(&self) -> &OsStr {
+        debug_assert!(self.target.file_name().is_some());
+        self.target.file_name().unwrap()
+    }
+
     pub fn to_path_buf(&self) -> PathBuf {
         PathBuf::from(self)
+    }
+
+    pub fn exists(&self) -> bool {
+        self.to_path_buf().exists()
     }
 }
 
 impl From<SysPath> for PathBuf {
     fn from(path: SysPath) -> Self {
-        let mut new_path = path.base;
+        let mut new_path = path.engine_paths().project_root().to_path_buf();
+        new_path.push(path.base);
         new_path.push(path.target);
         new_path
     }
@@ -101,7 +112,8 @@ impl From<SysPath> for PathBuf {
 
 impl From<&SysPath> for PathBuf {
     fn from(path: &SysPath) -> Self {
-        let mut new_path = path.base.clone();
+        let mut new_path = path.engine_paths().project_root().to_path_buf();
+        new_path.push(path.base.clone());
         new_path.push(path.target.clone());
         new_path
     }
@@ -139,10 +151,24 @@ impl TryFrom<HtmlPath> for MarkdownPath {
         if let Some(markdown_path) = html_path.src_file {
             Ok(markdown_path)
         } else {
-            Err(anyhow!(
-                "no markdown path associated with {}",
-                html_path.to_path_buf().display()
-            ))
+            let engine_paths = html_path.engine_paths();
+            let md_file_name = html_path.inner.with_extension("md");
+            let md_file_name = md_file_name.file_name();
+            let candidate = engine_paths
+                .project_root()
+                .join(engine_paths.src_root())
+                .join(md_file_name);
+
+            if candidate.exists() {
+                let src_path = engine_paths.src_root().join(md_file_name);
+                MarkdownPath::new(html_path.engine_paths(), src_path)
+            } else {
+                Err(anyhow!(
+                    "no markdown path associated with {} and unable to find markdown file at {}",
+                    html_path.to_path_buf().display(),
+                    candidate.display()
+                ))
+            }
         }
     }
 }
@@ -154,6 +180,30 @@ pub struct HtmlPath {
 }
 
 impl HtmlPath {
+    pub fn new<P: AsRef<Path>>(engine_paths: Arc<EnginePaths>, file_path: P) -> Result<Self> {
+        let file_path = file_path.as_ref();
+        if file_path.file_name().is_none() {
+            return Err(anyhow!(
+                "no file name present in file path: {}",
+                file_path.display()
+            ));
+        }
+
+        let sys_path = SysPath::new(engine_paths.clone(), engine_paths.output_root(), file_path)?;
+        dbg!(&sys_path);
+        if !sys_path.exists() {
+            return Err(anyhow!(
+                "attempt to make new HtmlPath from nonexistent file: {}",
+                sys_path.to_path_buf().display()
+            ));
+        }
+
+        Ok(Self {
+            inner: sys_path,
+            src_file: None,
+        })
+    }
+
     impl_engine_paths!(inner);
 }
 
@@ -173,9 +223,51 @@ impl From<MarkdownPath> for HtmlPath {
     }
 }
 
-pub struct RelativeUri;
+#[derive(Debug, Clone)]
+pub struct RelativeUri {
+    uri: String,
+    initiator: HtmlPath,
+}
 
-pub struct AbsoluteUri;
+impl RelativeUri {
+    pub fn new<S: Into<String>>(initiator: &HtmlPath, uri: S) -> Result<Self> {
+        let uri = uri.into();
+        if uri.starts_with('/') {
+            return Err(anyhow!(
+                "cannot create new relative URI from an absolute URI"
+            ));
+        }
+        if uri.trim().is_empty() {
+            return Err(anyhow!("no URI provided"));
+        }
+        Ok(Self {
+            uri,
+            initiator: initiator.clone(),
+        })
+    }
+}
+
+pub struct AbsoluteUri {
+    uri: String,
+    initiator: HtmlPath,
+}
+impl AbsoluteUri {
+    pub fn new<S: Into<String>>(initiator: &HtmlPath, uri: S) -> Result<Self> {
+        let uri = uri.into();
+        if !uri.starts_with('/') {
+            return Err(anyhow!(
+                "cannot create new absolute URI from a relative URI"
+            ));
+        }
+        if uri.len() < 2 {
+            return Err(anyhow!("no URI provided"));
+        }
+        Ok(Self {
+            uri,
+            initiator: initiator.clone(),
+        })
+    }
+}
 
 pub struct WorkingDir;
 
@@ -221,33 +313,178 @@ mod test {
     #[test]
     fn makes_markdown_path_with_valid_paths() {
         let (paths, tree) = simple_init();
-        let md_path = MarkdownPath::new(paths.clone(), "src/page.md")
+        let md_path = MarkdownPath::new(paths, "src/page.md")
             .expect("should be able to make a markdown path");
     }
 
     #[test]
     fn markdown_path_creation_fails_when_outside_src() {
         let (paths, tree) = simple_init();
-        let md_path = MarkdownPath::new(paths.clone(), "outside/page.md");
+        let md_path = MarkdownPath::new(paths, "outside/page.md");
         assert!(md_path.is_err());
     }
 
     #[test]
     fn htmlpath_from_markdownpath() {
         let (paths, tree) = simple_init();
-        let md_path = MarkdownPath::new(paths.clone(), "src/page.md").unwrap();
+        let md_path = MarkdownPath::new(paths, "src/page.md").unwrap();
         let html_path = HtmlPath::from(md_path);
-        assert_eq!(html_path.to_path_buf(), PathBuf::from("target/page.html"));
+        assert_eq!(
+            html_path.to_path_buf(),
+            tree.path().join("target/page.html")
+        );
         assert!(html_path.src_file.is_some());
     }
 
     #[test]
-    fn markdownpath_tryfrom_htmlpath() {
+    fn make_new_htmlpath_when_file_exists() {
+        let tree = temptree! {
+          "rules.rhai": "",
+          templates: {},
+          target: {
+              "page.html": ""
+          },
+          src: {},
+          syntax_themes: {}
+        };
+        let paths = default_test_paths(&tree);
+        let html_path =
+            HtmlPath::new(paths, "target/page.html").expect("failed to make new html path");
+        assert_eq!(
+            html_path.to_path_buf(),
+            tree.path().join("target/page.html")
+        );
+        assert!(html_path.src_file.is_none());
+    }
+
+    #[test]
+    fn fails_to_make_new_htmlpath_when_file_does_not_exists() {
+        let tree = temptree! {
+          "rules.rhai": "",
+          templates: {},
+          target: {},
+          src: {},
+          syntax_themes: {}
+        };
+        let paths = default_test_paths(&tree);
+        let html_path = HtmlPath::new(paths, "target/page.html");
+        assert!(html_path.is_err());
+    }
+
+    #[test]
+    fn markdownpath_tryfrom_htmlpath_fails_when_markdown_file_doesnt_exist() {
+        let tree = temptree! {
+          "rules.rhai": "",
+          templates: {},
+          target: {
+              "page.html": "",
+          },
+          src: {},
+          syntax_themes: {}
+        };
+        let paths = default_test_paths(&tree);
+        let html_path = HtmlPath::new(paths, "target/page.html").unwrap();
+        let new_md_path = MarkdownPath::try_from(html_path);
+        assert!(new_md_path.is_err());
+    }
+
+    #[test]
+    fn markdownpath_tryfrom_htmlpath_when_markdown_file_exists() {
+        let tree = temptree! {
+          "rules.rhai": "",
+          templates: {},
+          target: {
+              "page.html": "",
+          },
+          src: {
+              "page.md": "",
+          },
+          syntax_themes: {}
+        };
+        let paths = default_test_paths(&tree);
+
+        let html_path = HtmlPath::new(paths, "target/page.html").unwrap();
+        let new_md_path =
+            MarkdownPath::try_from(html_path).expect("failed to create new markdown path");
+    }
+
+    #[test]
+    fn markdownpath_tryfrom_htmlpath_when_option_is_some() {
         let (paths, tree) = simple_init();
-        let md_path = MarkdownPath::new(paths.clone(), "src/page.md").unwrap();
+        let md_path = MarkdownPath::new(paths, "src/page.md").unwrap();
         let html_path = HtmlPath::from(md_path.clone());
         let new_md_path = MarkdownPath::try_from(html_path)
             .expect("failed to convert htmlpath back to markdown path");
         assert_eq!(new_md_path.to_path_buf(), md_path.to_path_buf());
+    }
+
+    #[test]
+    fn makes_new_relative_uri() {
+        let (paths, tree) = simple_init();
+        let md_path = MarkdownPath::new(paths, "src/page.md").unwrap();
+        let html_path = HtmlPath::from(md_path);
+
+        let relative_uri =
+            RelativeUri::new(&html_path, "img/sample.png").expect("failed to make relative URI");
+    }
+
+    #[test]
+    fn relative_uri_fails_if_using_absolute_uri() {
+        let (paths, tree) = simple_init();
+        let md_path = MarkdownPath::new(paths, "src/page.md").unwrap();
+        let html_path = HtmlPath::from(md_path);
+
+        let relative_uri = RelativeUri::new(&html_path, "/img/sample.png");
+        assert!(relative_uri.is_err());
+    }
+
+    #[test]
+    fn relative_uri_fails_if_empty() {
+        let (paths, tree) = simple_init();
+        let md_path = MarkdownPath::new(paths, "src/page.md").unwrap();
+        let html_path = HtmlPath::from(md_path);
+
+        let relative_uri = RelativeUri::new(&html_path, "");
+        assert!(relative_uri.is_err());
+    }
+
+    #[test]
+    fn makes_new_absolute_uri() {
+        let (paths, tree) = simple_init();
+        let md_path = MarkdownPath::new(paths, "src/page.md").unwrap();
+        let html_path = HtmlPath::from(md_path);
+
+        let absolute_uri =
+            AbsoluteUri::new(&html_path, "/img/sample.png").expect("failed to make absolute URI");
+    }
+
+    #[test]
+    fn absolute_uri_fails_if_using_relative_uri() {
+        let (paths, tree) = simple_init();
+        let md_path = MarkdownPath::new(paths, "src/page.md").unwrap();
+        let html_path = HtmlPath::from(md_path);
+
+        let absolute_uri = AbsoluteUri::new(&html_path, "img/sample.png");
+        assert!(absolute_uri.is_err());
+    }
+
+    #[test]
+    fn absolute_uri_fails_if_blank() {
+        let (paths, tree) = simple_init();
+        let md_path = MarkdownPath::new(paths, "src/page.md").unwrap();
+        let html_path = HtmlPath::from(md_path);
+
+        let absolute_uri = AbsoluteUri::new(&html_path, "");
+        assert!(absolute_uri.is_err());
+    }
+
+    #[test]
+    fn absolute_uri_fails_if_nothing_after_slash() {
+        let (paths, tree) = simple_init();
+        let md_path = MarkdownPath::new(paths, "src/page.md").unwrap();
+        let html_path = HtmlPath::from(md_path);
+
+        let absolute_uri = AbsoluteUri::new(&html_path, "/");
+        assert!(absolute_uri.is_err());
     }
 }
