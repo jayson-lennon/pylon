b5a5ebf8a4f233ee23438ba177cc86e73f51b560
Jayson Lennon <jayson@jaysonlennon.dev>
2022-05-29 14:12:55 -0700

    Add TypedPath and ConfirmedPath

---
+++ crates/typed_path/src/marker.rs
@@ -0,0 +1,110 @@
+use crate::{Result, TypedPath};
+
+pub trait PathMarker: Clone {
+    fn confirm<T: PathMarker>(&self, path: &TypedPath<T>) -> Result<bool>;
+}
+
+#[derive(Clone, Debug)]
+struct File;
+impl PathMarker for File {
+    fn confirm<T: PathMarker>(&self, path: &TypedPath<T>) -> Result<bool> {
+        if path.inner().is_file() {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Dir;
+impl PathMarker for Dir {
+    fn confirm<T: PathMarker>(&self, path: &TypedPath<T>) -> Result<bool> {
+        if path.inner().is_dir() {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::ffi::OsStr;
+
+    use crate::test::{abs, rel};
+    use crate::{AbsPath, Result, SysPath, TypedPath};
+    use temptree::temptree;
+
+    use super::*;
+
+    use crate::marker;
+
+    #[test]
+    fn file_path_marker() {
+        let tree = temptree! {
+            "test_file": ""
+        };
+        let root = AbsPath::new(tree.path()).unwrap();
+        let sys_path = SysPath::new(&root, rel!(""), rel!("test_file"));
+        let file = TypedPath::new(sys_path, marker::File);
+        file.confirm()
+            .expect("should be able to confirm that a file exists");
+    }
+
+    #[test]
+    fn file_path_marker_fails_when_not_a_file() {
+        let tree = temptree! {
+            test_dir: {}
+        };
+        let root = AbsPath::new(tree.path()).unwrap();
+        let sys_path = SysPath::new(&root, rel!(""), rel!("test_dir"));
+        let file = TypedPath::new(sys_path, marker::File);
+        let confirmed = file.confirm();
+        assert!(confirmed.is_err())
+    }
+
+    #[test]
+    fn file_path_marker_fails_when_doesnt_exist() {
+        let tree = temptree! {};
+        let root = AbsPath::new(tree.path()).unwrap();
+        let sys_path = SysPath::new(&root, rel!(""), rel!("not_found"));
+        let file = TypedPath::new(sys_path, marker::File);
+        let confirmed = file.confirm();
+        assert!(confirmed.is_err())
+    }
+
+    #[test]
+    fn dir_path_marker() {
+        let tree = temptree! {
+            test: {}
+        };
+        let root = AbsPath::new(tree.path()).unwrap();
+        let sys_path = SysPath::new(&root, rel!(""), rel!("test"));
+        let dir = TypedPath::new(sys_path, marker::Dir);
+        dir.confirm()
+            .expect("should be able to confirm that a dir exists");
+    }
+
+    #[test]
+    fn dir_path_marker_fails_when_not_a_dir() {
+        let tree = temptree! {
+            "test_file": "",
+        };
+        let root = AbsPath::new(tree.path()).unwrap();
+        let sys_path = SysPath::new(&root, rel!(""), rel!("test_file"));
+        let dir = TypedPath::new(sys_path, marker::Dir);
+        let confirmed = dir.confirm();
+        assert!(confirmed.is_err())
+    }
+
+    #[test]
+    fn dir_path_marker_fails_when_doesnt_exist() {
+        let tree = temptree! {};
+        let root = AbsPath::new(tree.path()).unwrap();
+        let sys_path = SysPath::new(&root, rel!(""), rel!("not_found"));
+        let dir = TypedPath::new(sys_path, marker::Dir);
+        let confirmed = dir.confirm();
+        assert!(confirmed.is_err())
+    }
+}
